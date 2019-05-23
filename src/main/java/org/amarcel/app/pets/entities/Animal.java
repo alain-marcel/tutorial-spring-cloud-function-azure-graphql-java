773e2b3aac592d82e95b157036b69b06862121c2
amarcel <alain.marcel@lpgsystems.com>
2019-05-23 18:56:46 +0200

    This java version works even if Animal has 22 properties

---
+++ src/main/java/org/amarcel/app/pets/entities/Animal.java
@@ -0,0 +1,31 @@
+package org.amarcel.app.pets.entities;
+
+public class Animal {
+  public Animal() {};
+  
+  public String prop1;
+  public String prop2;
+  public String prop3;
+  public String prop4;
+  public String prop5;
+  public String prop6;
+  public String prop7;
+  public String prop8;
+  public String prop9;
+  public String prop10;
+  public String prop11;
+  public String prop12;
+  public String prop13;
+  public String prop14;
+  public String prop15;
+  public String prop16;
+  public String prop17;
+  public String prop18;
+  public String prop19;
+  public String prop20;
+  public String prop21;
+  public String prop22;
+//  public String prop23;
+//  public String prop24;
+//  public String prop25;
+}
--- src/main/java/org/amarcel/app/pets/resolvers/Query.java
+++ src/main/java/org/amarcel/app/pets/resolvers/Query.java
@@ -3,6 +3,8 @@ package org.amarcel.app.pets.resolvers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.amarcel.app.pets.entities.Age;
+import org.amarcel.app.pets.entities.Animal;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
@@ -12,6 +14,36 @@ import org.amarcel.app.pets.entities.Pet;
 @Component
 public class Query implements GraphQLQueryResolver {
 
+  public Animal animal() {
+    Animal a = new Animal();
+     a.prop1 = "pro1";
+     a.prop2 = "pro2";
+     a.prop3 = "pro3";
+     a.prop4 = "pro4";
+     a.prop5 = "pro5";
+     a.prop6 = "pro6";
+     a.prop7 = "pro7";
+     a.prop8 = "pro8";
+     a.prop9 = "pro9";
+     a.prop10 = "pro10";
+     a.prop11 = "pro11";
+     a.prop12 = "pro12";
+     a.prop13 = "pro13";
+     a.prop14 = "pro14";
+     a.prop15 = "pro15";
+     a.prop16 = "pro16";
+     a.prop17 = "pro17";
+     a.prop18 = "pro18";
+     a.prop19 = "pro19";
+     a.prop20 = "pro20";
+     a.prop21 = "pro21";
+     a.prop22 = "pro22";
+//     a.prop23 = "pro23";
+//     a.prop24 = "prop24";
+//     a.prop25 = "prop25";
+     return a;
+  } 
+  
   public List<Pet> pets()  {
     Pet pet1 = new Pet();
     pet1.id = 1L;
@@ -20,7 +52,7 @@ public class Query implements GraphQLQueryResolver {
 
     Pet pet2 = new Pet();
     pet2.id = 2L;
-    pet2.name = "Boul";
+    pet2.name = "Bob";
     pet2.age = 99;
 
     List<Pet> res = new ArrayList<>();
